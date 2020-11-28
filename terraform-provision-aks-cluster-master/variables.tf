358d03a4d7647636bd67b79e7e4f405f73203be5
theprojecthope <44763666+theprojecthope@users.noreply.github.com>
2020-11-28 21:51:07 +0530

    test cred added

--- terraform-provision-aks-cluster-master/variables.tf
+++ terraform-provision-aks-cluster-master/variables.tf
@@ -1,7 +1,9 @@
 variable "appId" {
+  default = "e6c27069-9d4c-426a-be6a-f5e716201a2a"
   description = "Azure Kubernetes Service Cluster service principal"
 }
 
 variable "password" {
+  default = "xWenKriEgoqLqX4OtOVQ_c8M7OX5hPYSxN"
   description = "Azure Kubernetes Service Cluster password"
 }
