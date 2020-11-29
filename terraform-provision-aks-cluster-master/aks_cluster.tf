402abbd8a5a5a7a05fd62863b19cc0f625ae3866
theprojecthope <44763666+theprojecthope@users.noreply.github.com>
2020-11-29 21:24:35 +0530

    Update aks_cluster.tf

--- terraform-provision-aks-cluster-master/aks_cluster.tf
+++ terraform-provision-aks-cluster-master/aks_cluster.tf
@@ -54,11 +54,3 @@ resource "azurerm_kubernetes_cluster" "aks_k2" {
     Environment = "Demo"
   }
 }
-
-output "client_certificate" {
-  value = "${azurerm_kubernetes_cluster.aks_k2.kube_config.0.client_certificate}"
-}
-
-output "kube_config" {
-  value = "${azurerm_kubernetes_cluster.aks_k2.kube_config_raw}"
-}
