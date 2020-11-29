a8e2ceb691ececf26b09008d5370542d2433762c
theprojecthope <44763666+theprojecthope@users.noreply.github.com>
2020-11-29 22:10:04 +0530

    Update k8s.tf

--- terraform-provision-aks-cluster-master/k8s.tf
+++ terraform-provision-aks-cluster-master/k8s.tf
@@ -1,10 +1,10 @@
 resource "azurerm_resource_group" "k8s" {
-  name     = "${var.resource_group_name}"
+  name     = "aks-demo"
   location = "${var.location}"
 }
 
 resource "azurerm_kubernetes_cluster" "k8s" {
-  name                = "${var.cluster_name}"
+  name                = "aksdemo"
   location            = "${azurerm_resource_group.k8s.location}"
   resource_group_name = "${azurerm_resource_group.k8s.name}"
   dns_prefix          = "${var.dns_prefix}"
